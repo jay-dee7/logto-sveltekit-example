f68008c84eb287da492081a28c22ad365bbee921
jay-dee7 <me@jsdp.dev>
2023-04-22 16:54:15 +0530

    add: env.example

--- src/routes/logto/+page.server.ts
+++ src/routes/logto/+page.server.ts
@@ -4,12 +4,10 @@ import type { Actions } from './$types';
 
 export const actions: Actions = {
 	signin: async ({ url, locals }) => {
-		console.log('inside logto signin method');
 		const uri = new URL('/logto/callback', url.href);
 		try {
 			throw await locals.logto.signIn(uri.toString());
 		} catch (err) {
-			console.log('we got something: ', err);
 			if (isSvelteKitRedirect(err)) {
 				return {
 					location: err.location
@@ -22,11 +20,9 @@ export const actions: Actions = {
 		}
 	},
 	signout: async ({ locals, url }) => {
-		console.log('inside signout');
 		try {
 			throw await locals.logto.signOut(url.origin);
 		} catch (err) {
-			console.log('err in signout: ', err);
 			if (isSvelteKitRedirect(err)) {
 				return { location: err.location };
 			}
